4011dabbda3229e7a95f4aff58036caced26d272
José Mendoza <joseantoniomendozanavarro@gmail.com>
2024-11-14 12:53:59 -0600

    added users api

---
+++ petfeast-api/user/tests/test_user_api.py
@@ -0,0 +1,30 @@
+"""
+Tests for the user API
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:create')
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+class PublicUserApiTests(TestCase):
+    """Test public features of the user API"""
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'name': 'test name'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload['email'])
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
