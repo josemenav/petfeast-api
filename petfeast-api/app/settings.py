bc51395a28b50260e425f9512a661bf85be26a40
José Mendoza <joseantoniomendozanavarro@gmail.com>
2024-11-25 21:20:29 -0600

    bug fix

--- petfeast-api/app/settings.py
+++ petfeast-api/app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'drf_spectacular',
     'django_celery_beat',
+    'corsheaders',
     'django_celery_results',
     'flower',
     'user',
@@ -58,7 +59,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'app.urls'
 
